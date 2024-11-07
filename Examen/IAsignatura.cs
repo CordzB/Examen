d7d7a7db6fa9ff7f727b38aad44de50180f089be
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-06 21:09:44 -0600

    Update IAsignatura.cs

--- Examen/IAsignatura.cs
+++ Examen/IAsignatura.cs
@@ -2,13 +2,14 @@
 
 namespace Examen
 {
-    internal class IAsignatura
+    internal interface IAsignatura
     {
         double CalcularNotaFinal();
         double CalcularNotaFinal(int n1, int n2, int n3);
         string MensajeNotaFinal(double notaFinal);
 
         void Imprimir();
+
 
 
 
