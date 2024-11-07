df45952efd091199a8fccdaf2cc9621bc85baf8e
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-06 18:53:30 -0600

    Create IAsignatura.cs

---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Examen
+{
+    internal class IAsignatura
+    {
+        double CalcularNotaFinal();
+        double CalcularNotaFinal(int n1, int n2, int n3);
+        string MensajeNotaFinal(double notaFinal);
+
+        void Imprimir();
+
+
+
+
+
+
+    }
+}
