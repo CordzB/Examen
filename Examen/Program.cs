cdffd9150d1c807a56dbb66d569e848d2ab21d69
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-06 21:05:09 -0600

    Update Program.cs

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Examen;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Asignatura asignatura = new Asignatura();
+
+        try
+        {
+            Console.Write("Ingrese el nombre del alumno: ");
+            asignatura.NombreAlumno = Console.ReadLine();
+
+            Console.Write("Ingrese el número de cuenta del alumno: ");
+            asignatura.NumeroCuenta = Console.ReadLine();
+
+            Console.Write("Ingrese el email del alumno: ");
+            asignatura.Email = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre de la asignatura: ");
+            asignatura.NombreAsignatura = Console.ReadLine();
+
+            Console.Write("Ingrese el horario de la asignatura: ");
+            asignatura.Horario = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre del docente: ");
+            asignatura.NombreDocente = Console.ReadLine();
+
+            Console.Write("Ingrese nota del primer parcial: ");
+            asignatura.N1 = int.Parse(Console.ReadLine());
+            if (asignatura.N1 > 30)
+            {
+                Console.WriteLine("La nota del primer parcial sobrepasa del 30%");
+                return;
+            }
+
+            Console.Write("Ingrese nota del segundo parcial: ");
+            asignatura.N2 = int.Parse(Console.ReadLine());
+            if (asignatura.N2 > 30)
+            {
+                Console.WriteLine("La nota del segundo parcial sobrepasa del 30%");
+                return;
+            }
+
+            Console.Write("Ingrese nota del tercer parcial: ");
+            asignatura.N3 = int.Parse(Console.ReadLine());
+            if (asignatura.N3 > 40)
+            {
+                Console.WriteLine("La nota del tercer parcial sobrepasa del 40%");
+                return;
+            }
+
+            asignatura.Imprimir();
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, ingrese datos numéricos donde se requiera.");
+        }
+    }
+}
+
