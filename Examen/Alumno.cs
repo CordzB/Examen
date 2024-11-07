251c210bf4671566f8839844a22386d8ad936b6c
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-06 18:46:50 -0600

    Create Alumno.cs

---
+++ Examen/Alumno.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Examen
+{
+    internal class Alumno
+    {
+public string NombreAlumno { get; set; }
+public string NumeroCuenta { get; set; }
+public string Email { get; set; }
+
+
+    }
+}
